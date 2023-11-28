cb0737fbced22166db004ecd28d952acd2a45379
Chalaya22 <chalayaolga22@gmail.com>
2023-11-28 19:20:11 +0200

    comit 27

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -4,7 +4,7 @@ import css from './Home.module.css';
 const Home = () => {
   return (
     <div className={css.container}>
-      <h1 className={css.title}>Welcom, users!</h1>
+      <h1 className={css.title}>Dear, users!</h1>
       <span className={css.span}>
         Please, Sign up or Log in to have access to the Phonebook!
       </span>
