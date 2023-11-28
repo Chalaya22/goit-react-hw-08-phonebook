407b75727044463a9b752e1bffae9111d4b50772
Chalaya22 <130228713+Chalaya22@users.noreply.github.com>
2023-11-28 13:28:20 +0000

    Deploying to gh-pages from @ Chalaya22/goit-react-hw-08-phonebook@14d819ae9274cbbe16bed24bc1a2ef6331439617 🚀

---
+++ static/js/531.bd1edd39.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[531],{4531:function(e,n,s){s.r(n),s.d(n,{default:function(){return r}});s(2791);var a="Home_container__nnEVr",t="Home_span__6FJZF",o="Home_title__k+YSg",c=s(184),r=function(){return(0,c.jsxs)("div",{className:a,children:[(0,c.jsx)("h1",{className:o,children:"Welcom, users!"}),(0,c.jsx)("span",{className:t,children:"Please, Sign up or Log in to have access to the Phonebook!"})]})}}}]);
+//# sourceMappingURL=531.bd1edd39.chunk.js.map
